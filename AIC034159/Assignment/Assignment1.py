d7eaa98175fcf6a28b8f39b914bce4866edcf6cf
Fawwad Ahmed <30432008+fawwad123@users.noreply.github.com>
2019-05-13 10:52:21 -0700

    Update comments

--- AIC034159/Assignment/Assignment1.py
+++ AIC034159/Assignment/Assignment1.py
@@ -1,15 +1,18 @@
+# Write a program to calculate basic calculation of two numbers
+
 # Input 2 number and operator from the user
 num1 = float(input("Enter first number: "))
 num2 = float(input("Enter second number: "))
 operator = input("Enter operator: ")
 
+# Execute the calculation to 2 numberes with respect to operator
 if (operator == "+") :
     print(str(num1) + " + " + str(num2) + " = " + str(num1 + num2))
 elif (operator == "-") :
     print(str(num1) + " - " + str(num2) + " = " + str(num1 - num2))
 elif (operator == "*") :
     print(str(num1) + " * " + str(num2) + " = " + str(num1 * num2))
-elif (operator == "-") :
+elif (operator == "/") :
     print(str(num1) + " / " + str(num2) + " = " + str(num1 / num2))
-
-    
+else :
+    print("Invalid operator")
